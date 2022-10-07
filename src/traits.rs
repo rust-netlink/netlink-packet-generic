6d66beff8fe7bae71ff68c3035b4376b6a20f273
Gris Ge <fge@redhat.com>
2022-10-07 22:04:51 +0800

    Run cargo fmt
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/traits.rs
+++ src/traits.rs
@@ -5,7 +5,8 @@
 /// Provide the definition for generic netlink family
 ///
 /// Family payload type should implement this trait to provide necessary
-/// informations in order to build the packet headers (`nlmsghdr` and `genlmsghdr`).
+/// informations in order to build the packet headers (`nlmsghdr` and
+/// `genlmsghdr`).
 ///
 /// If you are looking for an example implementation, you can refer to the
 /// [`crate::ctrl`] module.
@@ -18,11 +19,12 @@ pub trait GenlFamily {
     /// Return the assigned family ID
     ///
     /// # Note
-    /// The implementation of generic family should assign the ID to `GENL_ID_GENERATE` (0x0).
-    /// So the controller can dynamically assign the family ID.
+    /// The implementation of generic family should assign the ID to
+    /// `GENL_ID_GENERATE` (0x0). So the controller can dynamically assign
+    /// the family ID.
     ///
-    /// Regarding to the reason above, you should not have to implement the function
-    /// unless the family uses static ID.
+    /// Regarding to the reason above, you should not have to implement the
+    /// function unless the family uses static ID.
     fn family_id(&self) -> u16 {
         0
     }
